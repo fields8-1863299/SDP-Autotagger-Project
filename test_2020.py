691a2bfd046feb1378761e6a164b80c7200d0ae9
Sam Fields <fields8@uw.edu>
2020-02-23 11:39:15 -0800

    remade test file

---
+++ test_2020.py
@@ -0,0 +1,18 @@
+import autotagger
+from config import AutotaggerConfiguration
+
+# follows the most basic process to read in a txt file and print out a tei xml file
+def print_tei(input):
+    # cfg -> configuration
+    cfg = AutotaggerConfiguration()
+    file = open(input, encoding="utf-8")
+    lines = file.readlines()
+    file.close()
+    # lines is a big (collection of) string(s)
+    tf = autotagger.TranscriptionFile(lines)
+    document = autotagger.setup_DOM(cfg)
+    # autotagger.run() -> builds the xml file
+    document = autotagger.run(tf, cfg)
+    print(document.toprettyxml('\t', '\n', None))
+
+print_tei("texts/d48_short.txt")
